9c14386b791600a5357ea2d1b1fd6d739caa7b08
Michał Mazur <mmazur@spoton.com>
2021-05-11 13:35:59 +0200

    Allow aborting status change by raising exception in pre_ method

--- fsm/exceptions.py
+++ fsm/exceptions.py
@@ -1,7 +1,13 @@
-__all__ = ["InvalidTransition"]
+__all__ = ["InvalidTransition", "AbortTransition"]
 
 
 class InvalidTransition(Exception):
     """Moving from an state to another is not possible."""
+
+    pass
+
+
+class AbortTransition(Exception):
+    """Changing state should be aborted"""
 
     pass
